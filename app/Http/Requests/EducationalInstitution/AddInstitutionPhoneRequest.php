b48629a769fe0ef5a35d4a68b205edad42d5b8cd
Mazin-Mubarak <mazin2322@gmail.com>
2021-11-23 15:02:39 +0200

    Feature: set authorization rule for add institution phone

--- app/Http/Requests/EducationalInstitution/AddInstitutionPhoneRequest.php
+++ app/Http/Requests/EducationalInstitution/AddInstitutionPhoneRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Requests\EducationalInstitution;
 
+use App\Models\EducationalInstitution;
 use App\Models\PhoneNumber;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Route;
 
 class AddInstitutionPhoneRequest extends FormRequest
 {
@@ -15,11 +17,27 @@ class AddInstitutionPhoneRequest extends FormRequest
     public function authorize()
     {
         /** the user can add phone number if
-         * # he created the institution
+         * 
          * # he is an supervisor in the institution
         */
         //TODO : set the authorization rules
-        return true;
+        $user = auth()->user();
+        $institutionId =  Route::getCurrentRoute()->parameter('id');
+
+        $institution = EducationalInstitution::find($institutionId);
+        if(!$institution){
+            //if institution does not exist allow it to be handled by the controller
+            return true;
+        }
+
+        //check if the user created the institution
+        //if so allow him to add the phone number
+
+        if($institution->created_by == $user->id){
+            return true;
+        }
+
+        return false;
     }
 
     /**
